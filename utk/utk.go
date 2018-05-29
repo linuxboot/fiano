bf91cc1e0a6c31ce6809834737497cec72d8994e
Gan Shun Lim <ganshun@gmail.com>
2018-05-29 11:35:20 -0700

    Changing utk to have support for multiple subcommands
    The idea is that in the future, you'll do something like utk parse
    or utk extract <rom>
    
    Signed-off-by: Gan Shun Lim <ganshun@gmail.com>

--- utk/utk.go
+++ utk/utk.go
@@ -1,40 +1,79 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
-	"encoding/json"
 
+	"github.com/google/subcommands"
 	"github.com/linuxboot/fiano/uefi"
 )
 
-func main() {
-	flag.Parse()
-	if len(flag.Args()) == 0 {
-		log.Fatal("A file name is required")
+// Parse subcommand
+type parseCmd struct {
+}
+
+func (*parseCmd) Name() string {
+	return "parse"
+}
+
+func (*parseCmd) Synopsis() string {
+	return "Parse rom file and print JSON summary to stdout"
+}
+
+func (*parseCmd) Usage() string {
+	return "parse <path-to-rom-file>\n"
+}
+
+func (*parseCmd) SetFlags(_ *flag.FlagSet) {}
+
+func (*parseCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+	args := f.Args()
+	if len(args) == 0 {
+		log.Print("A file name is required")
+		return subcommands.ExitUsageError
 	}
-	romfile := flag.Args()[0]
+
+	romfile := args[0]
 	buf, err := ioutil.ReadFile(romfile)
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		return subcommands.ExitFailure
 	}
+
 	flash, err := uefi.Parse(buf)
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		return subcommands.ExitFailure
 	}
 	errlist := flash.Validate()
 	for _, err := range errlist {
-		fmt.Printf("Error found: %v\n", err.Error())
+		log.Printf("Error found: %v\n", err.Error())
 	}
 	if len(errlist) > 0 {
-		os.Exit(1)
+		return subcommands.ExitFailure
 	}
-	b, err:= json.MarshalIndent(flash, "", "    ")
+
+	b, err := json.MarshalIndent(flash, "", "    ")
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		return subcommands.ExitFailure
 	}
 	fmt.Println(string(b))
+	return subcommands.ExitSuccess
+}
+
+func main() {
+	subcommands.Register(subcommands.HelpCommand(), "")
+	subcommands.Register(subcommands.FlagsCommand(), "")
+	subcommands.Register(subcommands.CommandsCommand(), "")
+	subcommands.Register(&parseCmd{}, "")
+	flag.Parse()
+
+	ctx := context.Background()
+	os.Exit(int(subcommands.Execute(ctx)))
 }
