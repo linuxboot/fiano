a9a8e7d73d26421cbd29565b7e4fe06ab600dee1
Gan Shun Lim <ganshun@gmail.com>
2018-06-07 18:40:16 -0700

    Reorganize Region
    The intention is eventually for the Flash type to hold all the regions,
    and for each region to point back to the Region struct in the IFD
    
    Signed-off-by: Gan Shun Lim <ganshun@gmail.com>

--- uefi/biosregion.go
+++ uefi/biosregion.go
@@ -5,41 +5,49 @@ import (
 	"strings"
 )
 
-// BiosRegion represents the Bios Region in the firmware.
+// BIOSRegion represents the Bios Region in the firmware.
 // It holds all the FVs as well as padding
 // TODO(ganshun): handle padding
-type BiosRegion struct {
+type BIOSRegion struct {
+	// holds the raw data
+	buf             []byte
 	FirmwareVolumes []FirmwareVolume
+
+	//Metadata for extraction and recovery
+	ExtractPath string
+	// This is a pointer to the Region struct laid out in the ifd
+	Position *Region
 }
 
 // Summary prints a multi-line description of the Bios Region
-func (br BiosRegion) Summary() string {
+func (br BIOSRegion) Summary() string {
 	var fvols []string
 	for _, fv := range br.FirmwareVolumes {
 		fvols = append(fvols, fv.Summary())
 	}
-	return fmt.Sprintf("BiosRegion{\n"+
+	return fmt.Sprintf("BIOSRegion{\n"+
 		"    FirmwareVolumes=[\n"+
 		"        %v\n"+
 		"    ]\n"+
 		"}", Indent(strings.Join(fvols, "\n"), 8))
 }
 
-// NewBiosRegion parses a sequence of bytes and returns a BiosRegion
-// object, if a valid one is passed, or an error
-func NewBiosRegion(data []byte) (*BiosRegion, error) {
-	var br BiosRegion
+// NewBIOSRegion parses a sequence of bytes and returns a BIOSRegion
+// object, if a valid one is passed, or an error. It also points to the
+// Region struct uncovered in the ifd.
+func NewBIOSRegion(buf []byte, r *Region) (*BIOSRegion, error) {
+	br := BIOSRegion{buf: buf, Position: r}
 	for {
-		offset := FindFirmwareVolumeOffset(data)
+		offset := FindFirmwareVolumeOffset(buf)
 		if offset == -1 {
 			// no firmware volume found, stop searching
 			break
 		}
-		fv, err := NewFirmwareVolume(data[offset:])
+		fv, err := NewFirmwareVolume(buf[offset:])
 		if err != nil {
 			return nil, err
 		}
-		data = data[uint64(offset)+fv.Length:]
+		buf = buf[uint64(offset)+fv.Length:]
 		br.FirmwareVolumes = append(br.FirmwareVolumes, *fv)
 		// FIXME remove the `break` and move the offset to the next location to
 		// search for FVs (i.e. offset + fv.size)
