e3555575c923d66f1c24c8f60eddbc6dcabaa7dd
Gan Shun <ganshun@gmail.com>
2018-06-24 16:10:55 -0700

    Json UUID (#22)
    Signed-off-by: Gan Shun Lim <ganshun@gmail.com>

--- uuid/uuid_test.go
+++ uuid/uuid_test.go
@@ -14,6 +14,11 @@ var (
 	shortUUIDString     = "0123456789ABCDEF0123456789ABCDEF"
 	badUUIDStringLength = "01234567"
 	badHex              = "GHGHGHGHGHGHGH"
+
+	// UUID JSON examples
+	exampleJSON       = `{"UUID" : "` + exampleUUIDString + `"}`
+	exampleJSONBadHex = `{"UUID" : "` + badHex + `"}`
+	exampleJSONBadKey = `{"UU" : "` + exampleUUIDString + `"}`
 )
 
 func TestParse(t *testing.T) {
@@ -42,6 +47,54 @@ func TestParse(t *testing.T) {
 			}
 		} else if *u != *test.u {
 			t.Errorf("UUID was parsed incorrectly, expected %v\n, got %v\n, string was %v", test.u, u, test.s)
+		}
+	}
+}
+
+func TestMarshal(t *testing.T) {
+	var tests = []struct {
+		j string
+		u *UUID
+	}{
+		{exampleJSON, &exampleUUID},
+	}
+	for _, test := range tests {
+		j, err := test.u.MarshalJSON()
+		if err != nil {
+			t.Errorf("No error was expected, got %v", err)
+		}
+		if test.j != string(j) {
+			t.Errorf("JSON strings are not equal. Expected:\n%v\ngot:\n%v", test.j, string(j))
+		}
+	}
+}
+
+func TestUnmarshal(t *testing.T) {
+	var tests = []struct {
+		j   string
+		u   *UUID
+		msg string
+	}{
+		{exampleJSON, &exampleUUID, ""},
+		{exampleJSONBadHex, nil, fmt.Sprintf("uuid string not correct, need string of the format \n%v\n, got \n%v",
+			UExample, badHex)},
+		{exampleJSONBadKey, nil, fmt.Sprintf("uuid string has incorrect length, need string of the format \n%v\n, got \n%v",
+			UExample, "")},
+	}
+	for _, test := range tests {
+		var g UUID
+		err := g.UnmarshalJSON([]byte(test.j))
+		if test.msg == "" && err != nil {
+			t.Errorf("No error was expected, got %v", err)
+		}
+		if test.msg != "" && err == nil {
+			t.Errorf("Error was expected: %v, got nil", test.msg)
+		}
+		if err != nil && err.Error() != test.msg {
+			t.Errorf("Got Error msg %v, was expecting %v", err.Error(), test.msg)
+		}
+		if test.u != nil && *test.u != g {
+			t.Errorf("UUIDs are not equal. Expected:\n%v\ngot:\n%v", test.u, g)
 		}
 	}
 }
