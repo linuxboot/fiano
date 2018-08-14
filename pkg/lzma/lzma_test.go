967dbb4293f96149ea8c019c75cdc178031778ae
Ryan O'Leary <ryanoleary@google.com>
2018-08-14 14:23:04 -0700

    Add lzma compression package
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

---
+++ pkg/lzma/lzma_test.go
@@ -0,0 +1,49 @@
+package lzma
+
+import (
+	"io/ioutil"
+	"reflect"
+	"testing"
+)
+
+func TestEncode(t *testing.T) {
+	// Read test data.
+	want, err := ioutil.ReadFile("testdata/data.bin")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Encoded and decode
+	encoded, err := Encode(want)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := Decode(encoded)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("decompressed image did not match, (got: %d bytes, want: %d bytes)", len(got), len(want))
+	}
+}
+
+func TestDecode(t *testing.T) {
+	// Read test data.
+	want, err := ioutil.ReadFile("testdata/data.bin")
+	if err != nil {
+		t.Fatal(err)
+	}
+	encoded, err := ioutil.ReadFile("testdata/data.bin.lzma")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Decode
+	got, err := Decode(encoded)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("decompressed image did not match, (got: %d bytes, want: %d bytes)", len(got), len(want))
+	}
+}
