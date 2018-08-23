84680f402449488717e9f2b70bdf5fb5a3bb50ce
Ryan O'Leary <ryanoleary@google.com>
2018-08-23 15:57:17 -0700

    Add copyright header to each file
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- uuid/uuid.go
+++ uuid/uuid.go
@@ -1,3 +1,7 @@
+// Copyright 2018 the LinuxBoot Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package uuid implements the mixed-endian UUID as implemented by Microsoft.
 package uuid
 
