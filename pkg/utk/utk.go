49d2fa25a7edf9882fb57688f9d3a5e210782958
Ryan O'Leary <ryanoleary@google.com>
2018-09-17 03:47:37 +0200

    Add missing licenses
    Signed-off-by: Ryan O'Leary <ryanoleary@google.com>

--- pkg/utk/utk.go
+++ pkg/utk/utk.go
@@ -1,3 +1,7 @@
+// Copyright 2018 the LinuxBoot Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package utk
 
 // Package utk is where the implementation of the utk command lives.
