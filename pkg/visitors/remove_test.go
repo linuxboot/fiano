31e880617cad70662c4b2e492814e277d8927511
Gan Shun Lim <ganshun@gmail.com>
2018-10-29 16:00:56 -0700

    Add remove_pad cli option
    Fixes #204
    Also fixes a bug where I didn't set the string type in file.go
    
    Signed-off-by: Gan Shun Lim <ganshun@gmail.com>

--- pkg/visitors/remove_test.go
+++ pkg/visitors/remove_test.go
@@ -8,12 +8,55 @@ import (
 	"testing"
 )
 
-func TestRemove(t *testing.T) {
+func TestRemoveNoPad(t *testing.T) {
 	f := parseImage(t)
 
+	count := &Count{}
+	if err := count.Run(f); err != nil {
+		t.Fatal(err)
+	}
+	padCount := count.FileTypeCount["EFI_FV_FILETYPE_FFS_PAD"]
 	// Apply the visitor.
 	remove := &Remove{
 		Predicate: FindFileGUIDPredicate(*testGUID),
+		Pad:       false,
+	}
+	if err := remove.Run(f); err != nil {
+		t.Fatal(err)
+	}
+
+	// We expect one match.
+	if len(remove.Matches) != 1 {
+		t.Fatalf("got %d matches; expected 1", len(remove.Matches))
+	}
+
+	// We expect no match.
+	results := find(t, f, testGUID)
+	if len(results) != 0 {
+		t.Errorf("got %d matches; expected 0", len(results))
+	}
+	// We expect the same number of pad files
+	if err := count.Run(f); err != nil {
+		t.Fatal(err)
+	}
+	if newPadCount := count.FileTypeCount["EFI_FV_FILETYPE_FFS_PAD"]; padCount != newPadCount {
+		t.Errorf("differing number of pad files: originally had %v, after removal got %v",
+			padCount, newPadCount)
+	}
+}
+
+func TestRemovePad(t *testing.T) {
+	f := parseImage(t)
+
+	count := &Count{}
+	if err := count.Run(f); err != nil {
+		t.Fatal(err)
+	}
+	padCount := count.FileTypeCount["EFI_FV_FILETYPE_FFS_PAD"]
+	// Apply the visitor.
+	remove := &Remove{
+		Predicate: FindFileGUIDPredicate(*testGUID),
+		Pad:       true,
 	}
 	if err := remove.Run(f); err != nil {
 		t.Fatal(err)
@@ -28,5 +71,13 @@ func TestRemove(t *testing.T) {
 	results := find(t, f, testGUID)
 	if len(results) != 0 {
 		t.Fatalf("got %d matches; expected 0", len(results))
+	}
+	// We expect one more pad file
+	if err := count.Run(f); err != nil {
+		t.Fatal(err)
+	}
+	if newPadCount := count.FileTypeCount["EFI_FV_FILETYPE_FFS_PAD"]; padCount+1 != newPadCount {
+		t.Errorf("differing number of pad files: expected %v, got %v",
+			padCount+1, newPadCount)
 	}
 }
