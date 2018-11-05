0e546d0ca1a21fd5347eb0a3947ee33d120c9b5e
Ronald G. Minnich <rminnich@gmail.com>
2018-11-05 08:03:18 -0800

    Add a comment command
    I keep hitting the fact that this would make my current testing
    a LOT easier.
    
    I can have a whole list of remove commands, and I want to leave the name
    in there, so I don't forget I'm leaving it in, and changing
    remove xxx.dxe
    to
    comment xxx.dxe
    
    is just too handy. Until we have forth that is.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ pkg/visitors/comment.go
@@ -0,0 +1,36 @@
+// Copyright 2018 the LinuxBoot Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package visitors
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/linuxboot/fiano/pkg/uefi"
+)
+
+// Comment holds the io.Writer and args for a comment
+type Comment struct {
+	W io.Writer
+	s string
+}
+
+// Run wraps Visit and performs some setup and teardown tasks.
+func (v *Comment) Run(f uefi.Firmware) error {
+	fmt.Fprintf(v.W, "%s\n", v.s)
+	return nil
+}
+
+// Visit applies the Comment visitor to any Firmware type.
+func (v *Comment) Visit(f uefi.Firmware) error {
+	return nil
+}
+
+func init() {
+	RegisterCLI("comment", "Print one arg", 1, func(args []string) (uefi.Visitor, error) {
+		return &Comment{W: os.Stdout, s: args[0]}, nil
+	})
+}
