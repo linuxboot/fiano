964701cd9b445df4621f2dfe842cbfaaa6c35da5
Andrea Barberio <insomniac@slackware.it>
2019-07-13 19:21:42 +0100

    Added FSPInfo tool
    Signed-off-by: Andrea Barberio <insomniac@slackware.it>

---
+++ cmds/fspinfo/main_test.go
@@ -0,0 +1,93 @@
+// Copyright 2017-2018 the LinuxBoot Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"github.com/linuxboot/fiano/pkg/fsp"
+)
+
+const (
+	// From https://github.com/IntelFsp/FSP/blob/master/ApolloLakeFspBinPkg/FspBin/Fsp.fd
+	// under the FSP license. See README.md under `test_blobs`.
+	FSPTestFile = "test_blobs/ApolloLakeFspBinPkg/Fsp.fd"
+)
+
+func TestNewInfoHeaderRealFile(t *testing.T) {
+	buf, err := ioutil.ReadFile(FSPTestFile)
+	if err != nil {
+		t.Errorf("Error opening test file %s: %v", FSPTestFile, err)
+	}
+	// in the ApolloLake FSP, the FSP header starts at byte 148. This can be
+	// extracted by parsing it as uefi.FirmwareVolume, but it's not relevant for
+	// this test, so let's go with raw offsets.
+	hdr, err := fsp.NewInfoHeader(buf[148 : 148+fsp.HeaderV3Length])
+	if err != nil {
+		t.Errorf("NewInfoHeader failed to parse FSP file %s: %v", FSPTestFile, err)
+	}
+	if hdr.Signature != fsp.Signature {
+		t.Errorf("Invalid signature %v; want %v", hdr.Signature, fsp.Signature)
+	}
+	if hdr.HeaderLength != fsp.HeaderV3Length {
+		t.Errorf("Invalid header length %d; want %d", hdr.HeaderLength, fsp.HeaderV3Length)
+	}
+	if !bytes.Equal(hdr.Reserved1[:], bytes.Repeat([]byte{0}, 2)) {
+		t.Errorf("Invalid field Reserved1 %v; want %v", hdr.Reserved1, bytes.Repeat([]byte{0}, 2))
+	}
+	if hdr.SpecVersion != fsp.SpecVersion(0x20) {
+		t.Errorf("Invalid spec version %s; want %s", hdr.SpecVersion, fsp.SpecVersion(0x20))
+	}
+	if hdr.HeaderRevision != fsp.HeaderV3Revision {
+		t.Errorf("Invalid header revision %d; want %d", hdr.HeaderRevision, fsp.HeaderV3Revision)
+	}
+	if hdr.ImageRevision != fsp.ImageRevision(0x01040301) {
+		t.Errorf("Invalid image revision %s; want %s", hdr.ImageRevision, fsp.ImageRevision(0x1431))
+	}
+	if !bytes.Equal(hdr.ImageID[:], []byte("$APLFSP$")) {
+		t.Errorf("Invalid image ID %s; want %s", hdr.ImageID, "$APLFSP$")
+	}
+	if hdr.ImageSize != 0x2a000 {
+		t.Errorf("Invalid image size %#x; want %#x", hdr.ImageSize, 0x2a000)
+	}
+	if hdr.ImageBase != 0x200000 {
+		t.Errorf("Invalid image base %#x; want %#x", hdr.ImageSize, 0x200000)
+	}
+	if hdr.ImageAttribute != 0x1 {
+		t.Errorf("Invalid image attribute %#x; want %#x", hdr.ImageAttribute, 0x1)
+	}
+	if hdr.ComponentAttribute != 0x3003 {
+		t.Errorf("Invalid component attribute %#x; want %#x", hdr.ComponentAttribute, 0x3003)
+	}
+	if hdr.CfgRegionOffset != 0x124 {
+		t.Errorf("Invalid cfg region offset %#x; want %#x", hdr.CfgRegionOffset, 0x124)
+	}
+	if hdr.CfgRegionSize != 0x3b0 {
+		t.Errorf("Invalid cfg region size %#x; want %#x", hdr.CfgRegionSize, 0x3b0)
+	}
+	if !bytes.Equal(hdr.Reserved2[:], bytes.Repeat([]byte{0}, 4)) {
+		t.Errorf("Invalid field Reserved2 %v; want %v", hdr.Reserved2, bytes.Repeat([]byte{0}, 4))
+	}
+	if hdr.TempRAMInitEntryOffset != 0x0 {
+		t.Errorf("Invalid temp RAM init entry offset %#x; want %#x", hdr.TempRAMInitEntryOffset, 0x0)
+	}
+	if !bytes.Equal(hdr.Reserved3[:], bytes.Repeat([]byte{0}, 4)) {
+		t.Errorf("Invalid field Reserved3 %v; want %v", hdr.Reserved3, bytes.Repeat([]byte{0}, 4))
+	}
+	if hdr.NotifyPhaseEntryOffset != 0x580 {
+		t.Errorf("Invalid notify phase entry offset %#x; want %#x", hdr.NotifyPhaseEntryOffset, 0x580)
+	}
+	if hdr.FSPMemoryInitEntryOffset != 0x0 {
+		t.Errorf("Invalid FSP memory init entry offset %#x; want %#x", hdr.FSPMemoryInitEntryOffset, 0x0)
+	}
+	if hdr.TempRAMInitEntryOffset != 0x0 {
+		t.Errorf("Invalid temp RAM init entry offset %#x; want %#x", hdr.TempRAMInitEntryOffset, 0x0)
+	}
+	if hdr.FSPSiliconInitEntryOffset != 0x58a {
+		t.Errorf("Invalid FSP silicon init entry offset %#x; want %#x", hdr.FSPSiliconInitEntryOffset, 0x58a)
+	}
+}
---
+++ pkg/fsp/header.go
@@ -0,0 +1,251 @@
+// Copyright 2018 the LinuxBoot Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package fsp implements FSP info header parsing
+package fsp
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"strings"
+)
+
+// TODO support FSP versions < 2.0
+// TODO implement FSP_INFO_EXTENDED_HEADER
+
+// FSP 2.0 specification
+// https://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/fsp-architecture-spec-v2.pdf
+
+// values from the FSP 2.0 spec
+var (
+	Signature = [4]byte{'F', 'S', 'P', 'H'}
+)
+
+// constants from the FSP 2.0 spec
+const (
+	// size of the shared part of the header across FSP spec revisions
+	FixedInfoHeaderLength = 12
+	HeaderV3Length        = 72
+	// FSP 2.0
+	CurrentSpecVersion = SpecVersion(0x20)
+	HeaderV3Revision   = 3
+)
+
+// FixedInfoHeader is the common header among the various revisions of the FSP
+// info header.
+type FixedInfoHeader struct {
+	Signature      [4]byte
+	HeaderLength   uint32
+	Reserved1      [2]uint8
+	SpecVersion    SpecVersion
+	HeaderRevision uint8
+}
+
+// InfoHeaderRev3 represents the FSP_INFO_HEADER structure revision 3 (FSP
+// 2.0) as defined by Intel.
+type InfoHeaderRev3 struct {
+	FixedInfoHeader
+	ImageRevision             ImageRevision
+	ImageID                   [8]byte
+	ImageSize                 uint32
+	ImageBase                 uint32
+	ImageAttribute            ImageAttribute
+	ComponentAttribute        ComponentAttribute
+	CfgRegionOffset           uint32
+	CfgRegionSize             uint32
+	Reserved2                 [4]byte
+	TempRAMInitEntryOffset    uint32
+	Reserved3                 [4]byte
+	NotifyPhaseEntryOffset    uint32
+	FSPMemoryInitEntryOffset  uint32
+	TempRAMExitEntryOffset    uint32
+	FSPSiliconInitEntryOffset uint32
+}
+
+// Summary prints a multi-line summary of the header's content.
+func (ih InfoHeaderRev3) Summary() string {
+	s := fmt.Sprintf("Signature                   : %s\n", ih.Signature)
+	s += fmt.Sprintf("Header Length               : %d\n", ih.HeaderLength)
+	s += fmt.Sprintf("Reserved1                   : %#04x\n", ih.Reserved1)
+	s += fmt.Sprintf("Spec Version                : %s\n", ih.SpecVersion)
+	s += fmt.Sprintf("Header Revision             : %d\n", ih.HeaderRevision)
+	s += fmt.Sprintf("Image Revision              : %s\n", ih.ImageRevision)
+	s += fmt.Sprintf("Image ID                    : %s\n", ih.ImageID)
+	s += fmt.Sprintf("Image Size                  : %#08x %d\n", ih.ImageSize, ih.ImageSize)
+	s += fmt.Sprintf("Image Base                  : %#08x %d\n", ih.ImageBase, ih.ImageBase)
+	s += fmt.Sprintf("Image Attribute             : %s\n", ih.ImageAttribute)
+	s += fmt.Sprintf("Component Attribute         : %s\n", ih.ComponentAttribute)
+	s += fmt.Sprintf("Cfg Region Offset           : %#08x %d\n", ih.CfgRegionOffset, ih.CfgRegionOffset)
+	s += fmt.Sprintf("Cfg Region Size             : %#08x %d\n", ih.CfgRegionSize, ih.CfgRegionSize)
+	s += fmt.Sprintf("Reserved2                   : %#08x\n", ih.Reserved2)
+	s += fmt.Sprintf("TempRAMInit Entry Offset    : %#08x %d\n", ih.TempRAMInitEntryOffset, ih.TempRAMInitEntryOffset)
+	s += fmt.Sprintf("Reserved3                   : %#08x\n", ih.Reserved3)
+	s += fmt.Sprintf("NotifyPhase Entry Offset    : %#08x %d\n", ih.NotifyPhaseEntryOffset, ih.NotifyPhaseEntryOffset)
+	s += fmt.Sprintf("FSPMemoryInit Entry Offset  : %#08x %d\n", ih.FSPMemoryInitEntryOffset, ih.FSPMemoryInitEntryOffset)
+	s += fmt.Sprintf("TempRAMExit Entry Offset    : %#08x %d\n", ih.TempRAMExitEntryOffset, ih.TempRAMExitEntryOffset)
+	s += fmt.Sprintf("FSPSiliconInit Entry Offset : %#08x %d\n", ih.FSPSiliconInitEntryOffset, ih.FSPSiliconInitEntryOffset)
+	return s
+}
+
+// ImageRevision is the image revision field of the FSP info header.
+type ImageRevision uint32
+
+func (ir ImageRevision) String() string {
+	return fmt.Sprintf("%d.%d.%d.%d",
+		(ir>>24)&0xff,
+		(ir>>16)&0xff,
+		(ir>>8)&0xff,
+		ir&0xff,
+	)
+}
+
+// SpecVersion represents the spec version as a packed BCD two-digit,
+// dot-separated unsigned integer.
+type SpecVersion uint8
+
+func (sv SpecVersion) String() string {
+	return fmt.Sprintf("%d.%d", (sv>>4)&0x0f, sv&0x0f)
+}
+
+// ImageAttribute represents the image attributes.
+type ImageAttribute uint16
+
+func (ia ImageAttribute) String() string {
+	ret := fmt.Sprintf("%#04x ", uint16(ia))
+	if ia.IsGraphicsDisplaySupported() {
+		ret += "GraphicsDisplaySupported"
+	} else {
+		ret += "GraphicsDisplayNotSupported"
+	}
+	if uint16(ia) & ^(uint16(1)) != 0 {
+		ret += " (reserved bits are not zeroed)"
+	}
+	return ret
+}
+
+// IsGraphicsDisplaySupported returns true if FSP supports enabling graphics display.
+func (ia ImageAttribute) IsGraphicsDisplaySupported() bool {
+	return uint16(ia)&0x1 == 1
+}
+
+// Type identifies the FSP type.
+type Type uint8
+
+// FSP types. All the other values are reserved.
+var (
+	TypeT Type = 1
+	TypeM Type = 2
+	TypeS Type = 3
+	TypeO Type = 8
+	// TypeReserved is a fake type that represents a reserved FSP type.
+	TypeReserved Type
+)
+
+var fspTypeNames = map[Type]string{
+	TypeT:        "FSP-T",
+	TypeM:        "FSP-M",
+	TypeS:        "FSP-S",
+	TypeO:        "FSP-O",
+	TypeReserved: "FSP-ReservedType",
+}
+
+// ComponentAttribute represents the component attribute.
+type ComponentAttribute uint16
+
+// IsDebugBuild returns true if the FSP build is a debug build, and false
+// if it's a release build.
+func (ca ComponentAttribute) IsDebugBuild() bool {
+	return uint16(ca)&0x01 == 0
+}
+
+// IsTestRelease returns true if the release is a test release, and false if
+// it's an official release.
+func (ca ComponentAttribute) IsTestRelease() bool {
+	return uint16(ca)&0x03 == 0
+}
+
+// Type returns the FSP type.
+func (ca ComponentAttribute) Type() Type {
+	typ := Type(uint16(ca) >> 12)
+	if _, ok := fspTypeNames[typ]; ok {
+		return typ
+	}
+	return TypeReserved
+}
+
+func (ca ComponentAttribute) String() string {
+	var attrs []string
+	if ca.IsDebugBuild() {
+		attrs = append(attrs, "DebugBuild")
+	} else {
+		attrs = append(attrs, "ReleaseBuild")
+	}
+	if ca.IsTestRelease() {
+		attrs = append(attrs, "TestRelease")
+	} else {
+		attrs = append(attrs, "OfficialRelease")
+	}
+	if typeName, ok := fspTypeNames[ca.Type()]; ok {
+		attrs = append(attrs, typeName)
+	} else {
+		attrs = append(attrs, fmt.Sprintf("TypeUnknown(%v)", ca.Type()))
+	}
+	ret := fmt.Sprintf("%#04x %s", uint16(ca), strings.Join(attrs, "|"))
+	// bits 11:2 are reserved
+	if uint16(ca)&0x0ffe != 0 {
+		ret += " (reserved bits are not zeroed)"
+	}
+	return ret
+}
+
+// NewInfoHeader creates an InfoHeaderRev3 from a byte buffer.
+// TODO in the future this should return a generic info header, independently from
+//      the revision, but need to implement other header revisions first.
+func NewInfoHeader(b []byte) (*InfoHeaderRev3, error) {
+	if len(b) < FixedInfoHeaderLength {
+		return nil, fmt.Errorf("short FSP Info Header length %d; want at least %d", len(b), FixedInfoHeaderLength)
+	}
+	// TODO currently, only rev. 3 is supported
+	var f InfoHeaderRev3
+
+	reader := bytes.NewReader(b)
+	if err := binary.Read(reader, binary.LittleEndian, &f.FixedInfoHeader); err != nil {
+		return nil, err
+	}
+
+	// check signature
+	if !bytes.Equal(f.Signature[:], Signature[:]) {
+		return nil, fmt.Errorf("invalid signature %v; want %v", f.Signature, Signature)
+	}
+	// reserved bytes must be zero'ed
+	if !bytes.Equal(f.Reserved1[:], []byte{0, 0}) {
+		log.Printf("warning: reserved bytes must be zero, got %v", f.Reserved1)
+	}
+	// check spec version
+	// TODO currently, only FSP 2.0 is supported
+	if f.SpecVersion != CurrentSpecVersion {
+		return nil, fmt.Errorf("cannot handle spec version %s; want %s", f.SpecVersion, CurrentSpecVersion)
+	}
+	// check header revision
+	switch f.HeaderRevision {
+	case HeaderV3Revision:
+	default:
+		return nil, fmt.Errorf("cannot handle header revision %d; want %d", f.HeaderRevision, HeaderV3Revision)
+	}
+	switch f.HeaderLength {
+	case HeaderV3Length:
+	default:
+		return nil, fmt.Errorf("invalid header length %d; want %d", f.HeaderLength, HeaderV3Length)
+	}
+
+	// now that we know it's an info header spec 2.0, revision 3, re-read the
+	// buffer to fill the whole header.
+	reader = bytes.NewReader(b)
+	if err := binary.Read(reader, binary.LittleEndian, &f); err != nil {
+		return nil, err
+	}
+	return &f, nil
+}
