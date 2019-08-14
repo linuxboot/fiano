59425b5b8d5ca7cc7b232f6506e456f9c11b1727
Julien Viard de Galbert <julien.viard-de-galbert@itrenew.com>
2019-08-14 12:28:52 +0200

    visitor/table: Refactor printRow for layout view
    Signed-off-by: Julien Viard de Galbert <julien.viard-de-galbert@itrenew.com>

--- pkg/visitors/table.go
+++ pkg/visitors/table.go
@@ -35,38 +35,38 @@ func (v *Table) Visit(f uefi.Firmware) error {
 	switch f := f.(type) {
 	case *uefi.FlashImage:
 		v.depth = v.indent + 1
-		return v.printRow(f, "Image", "", "", 0, 0)
+		return v.printFirmware(f, "Image", "", "", 0, 0)
 	case *uefi.FirmwareVolume:
-		return v.printRow(f, "FV", f.FileSystemGUID.String(), "", v.offset+f.FVOffset, v.offset+f.FVOffset+f.DataOffset)
+		return v.printFirmware(f, "FV", f.FileSystemGUID.String(), "", v.offset+f.FVOffset, v.offset+f.FVOffset+f.DataOffset)
 	case *uefi.File:
 		// TODO: make name part of the file node
-		return v.printRow(f, "File", f.Header.GUID.String(), f.Header.Type, v.curOffset, v.curOffset+f.DataOffset)
+		return v.printFirmware(f, "File", f.Header.GUID.String(), f.Header.Type, v.curOffset, v.curOffset+f.DataOffset)
 	case *uefi.Section:
 		// Reset offset to O for (compressed) section content
-		return v.printRow(f, "Sec", f.String(), f.Type, v.curOffset, 0)
+		return v.printFirmware(f, "Sec", f.String(), f.Type, v.curOffset, 0)
 	case *uefi.FlashDescriptor:
 		v.depth = v.indent + 1
-		return v.printRow(f, "IFD", "", "", 0, 0)
+		return v.printFirmware(f, "IFD", "", "", 0, 0)
 	case *uefi.BIOSRegion:
 		v.depth = v.indent + 1
 		if f.FRegion != nil {
 			offset = uint64(f.FRegion.BaseOffset())
 		}
-		return v.printRow(f, "BIOS", "", "", offset, offset)
+		return v.printFirmware(f, "BIOS", "", "", offset, offset)
 	case *uefi.BIOSPadding:
-		return v.printRow(f, "BIOS Pad", "", "", v.offset+f.Offset, 0)
+		return v.printFirmware(f, "BIOS Pad", "", "", v.offset+f.Offset, 0)
 	case *uefi.NVarStore:
-		return v.printRow(f, "NVAR Store", "", "", v.curOffset, v.curOffset)
+		return v.printFirmware(f, "NVAR Store", "", "", v.curOffset, v.curOffset)
 	case *uefi.NVar:
-		return v.printRow(f, "NVAR", f.GUID.String(), f, v.curOffset, v.curOffset+uint64(f.DataOffset))
+		return v.printFirmware(f, "NVAR", f.GUID.String(), f, v.curOffset, v.curOffset+uint64(f.DataOffset))
 	case *uefi.RawRegion:
 		v.depth = v.indent + 1
 		if f.FRegion != nil {
 			offset = uint64(f.FRegion.BaseOffset())
 		}
-		return v.printRow(f, f.Type().String(), "", "", offset, offset)
+		return v.printFirmware(f, f.Type().String(), "", "", offset, offset)
 	default:
-		return v.printRow(f, fmt.Sprintf("%T", f), "", "", 0, 0)
+		return v.printFirmware(f, fmt.Sprintf("%T", f), "", "", 0, 0)
 	}
 }
 
@@ -74,7 +74,7 @@ func indent(n int) string {
 	return strings.Repeat(" ", n)
 }
 
-func (v *Table) printRow(f uefi.Firmware, node, name, typez interface{}, offset, dataOffset uint64) error {
+func (v *Table) printFirmware(f uefi.Firmware, node, name, typez interface{}, offset, dataOffset uint64) error {
 	if v.W == nil {
 		v.W = tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
 		defer func() { v.W.Flush() }()
@@ -90,14 +90,8 @@ func (v *Table) printRow(f uefi.Firmware, node, name, typez interface{}, offset,
 			typez = "(empty)"
 		}
 	}
-	if v.Layout {
-		if name == "" {
-			name = typez
-		}
-		fmt.Fprintf(v.W, "%s%v\t%v\t%#08x\t%#08x\n", indent(v.indent), node, name, offset, length)
-	} else {
-		fmt.Fprintf(v.W, "%s%v\t%v\t%v\t%#8x\n", indent(v.indent), node, name, typez, length)
-	}
+	v.printRow(node, name, typez, offset, length)
+
 	v2 := *v
 	v2.indent++
 	v2.offset = dataOffset
@@ -112,26 +106,28 @@ func (v *Table) printRow(f uefi.Firmware, node, name, typez interface{}, offset,
 	switch f := f.(type) {
 	case *uefi.FirmwareVolume:
 		// Print free space at the end of the volume
-		if v.Layout {
-			fmt.Fprintf(v.W, "%s%v\t%v\t%#08x\t%#08x\n", indent(v2.indent), "Free", "", offset+length-f.FreeSpace, f.FreeSpace)
-		} else {
-			fmt.Fprintf(v.W, "%s%v\t%v\t%v\t%#8x\n", indent(v2.indent), "Free", "", "", f.FreeSpace)
-		}
+		v2.printRow("Free", "", "", offset+length-f.FreeSpace, f.FreeSpace)
 	case *uefi.NVarStore:
 		// Print free space and GUID store
-		if v.Layout {
-			fmt.Fprintf(v.W, "%s%v\t%v\t%#08x\t%#08x\n", indent(v2.indent), "Free", "", offset+f.FreeSpaceOffset, f.GUIDStoreOffset-f.FreeSpaceOffset)
-			fmt.Fprintf(v.W, "%s%v\t%v\t%#08x\t%#08x\n", indent(v2.indent), "GUIDStore", fmt.Sprintf("%d GUID", len(f.GUIDStore)), offset+f.GUIDStoreOffset, f.Length-f.GUIDStoreOffset)
-		} else {
-			fmt.Fprintf(v.W, "%s%v\t%v\t%v\t%#8x\n", indent(v2.indent), "Free", "", "", f.GUIDStoreOffset-f.FreeSpaceOffset)
-			fmt.Fprintf(v.W, "%s%v\t%v\t%v\t%#8x\n", indent(v2.indent), "GUIDStore", "", fmt.Sprintf("%d GUID", len(f.GUIDStore)), f.Length-f.GUIDStoreOffset)
-		}
+		v2.printRow("Free", "", "", offset+f.FreeSpaceOffset, f.GUIDStoreOffset-f.FreeSpaceOffset)
+		v2.printRow("GUIDStore", "", fmt.Sprintf("%d GUID", len(f.GUIDStore)), offset+f.GUIDStoreOffset, f.Length-f.GUIDStoreOffset)
 	case *uefi.File:
 		// Align
 		// TODO: do we need the complex align logic from assemble?
 		v.curOffset = uefi.Align8(v.curOffset)
 	}
 	return nil
+}
+
+func (v *Table) printRow(node, name, typez interface{}, offset, length uint64) {
+	if v.Layout {
+		if name == "" {
+			name = typez
+		}
+		fmt.Fprintf(v.W, "%s%v\t%v\t%#08x\t%#08x\n", indent(v.indent), node, name, offset, length)
+	} else {
+		fmt.Fprintf(v.W, "%s%v\t%v\t%v\t%#8x\n", indent(v.indent), node, name, typez, length)
+	}
 }
 
 func init() {
