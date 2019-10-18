e5bcc38487988c5701d51bc0ea046c9cde09acf9
Julien Viard de Galbert <julien.viard-de-galbert@itrenew.com>
2019-10-18 10:25:53 +0200

    visitor/createfv: separate FV creation and FV insertion
    Signed-off-by: Julien Viard de Galbert <julien.viard-de-galbert@itrenew.com>

--- pkg/visitors/createfv.go
+++ pkg/visitors/createfv.go
@@ -78,120 +78,37 @@ func (v *CreateFV) Visit(f uefi.Firmware) error {
 			return fmt.Errorf("Cannot create FV at %#x (+%#x) no matching BIOS Pad found", v.AbsOffset, v.Size)
 		}
 		v.found = true
-		return v.createFV(f, offset, bp, idx)
+		fv, err := createEmptyFirmwareVolume(v.AbsOffset-offset, v.Size, &v.Name)
+		if err != nil {
+			return err
+		}
+		return insertFVinBP(f, v.AbsOffset-offset, bp, idx, fv)
 	}
 
 	return f.ApplyChildren(v)
 }
 
-func (v *CreateFV) createFV(br *uefi.BIOSRegion, offset uint64, bp *uefi.BIOSPadding, idx int) error {
+func insertFVinBP(br *uefi.BIOSRegion, offset uint64, bp *uefi.BIOSPadding, idx int, fv *uefi.FirmwareVolume) error {
 	// Copy the Elements before the modified BIOS Pad
 	newElements := make([]*uefi.TypedFirmware, idx, len(br.Elements))
 	copy(newElements, br.Elements[:idx])
 
 	bpBuf := bp.Buf()
 	// keep part of the BIOS Pad before the new fv if needed
-	if offset+bp.Offset < v.AbsOffset {
-		hbp, err := uefi.NewBIOSPadding(bpBuf[:v.AbsOffset-offset-bp.Offset], bp.Offset)
+	if bp.Offset < offset {
+		hbp, err := uefi.NewBIOSPadding(bpBuf[:offset-bp.Offset], bp.Offset)
 		if err != nil {
 			return err
 		}
 		newElements = append(newElements, uefi.MakeTyped(hbp))
 	}
 
-	// TODO: can this be refactored with the code in repack.go and assemble.go ?
-	fv := &uefi.FirmwareVolume{} // new Firmware Volume
-	// Set up volume header first.
-	fv.FileSystemGUID = *uefi.FFS2
-	fv.Signature = binary.LittleEndian.Uint32([]byte("_FVH"))
-	// TODO: retrieve all details from (all) other fv in BIOS Region
-	fv.Attributes = 0x0004FEFF
-	fv.Revision = 2
-	// Create Blocks
-	fv.Blocks = make([]uefi.Block, 2)
-	fv.Blocks[0] = uefi.Block{Size: 4096, Count: uint32(v.Size / 4096)}
-	fv.Blocks[1] = uefi.Block{}
-	// Calculate the HeaderLen field
-	fv.HeaderLen = uint16(uefi.FirmwareVolumeFixedHeaderSize + int(unsafe.Sizeof(uefi.Block{}))*len(fv.Blocks))
-
-	fv.DataOffset = uint64(fv.HeaderLen) // unless we add the extended header
-	fv.Length = v.Size
-
-	// TODO: should the extended header offset be computed ?
-	fv.ExtHeaderOffset = 0x60
-	fv.FVName = v.Name
-	fv.ExtHeaderSize = uefi.FirmwareVolumeExtHeaderMinSize
-
-	// Generate binary header.
-	header := new(bytes.Buffer)
-	err := binary.Write(header, binary.LittleEndian, fv.FirmwareVolumeFixedHeader)
-	if err != nil {
-		return fmt.Errorf("unable to construct binary header of new firmware volume: got %v", err)
-	}
-	for _, b := range fv.Blocks {
-		err = binary.Write(header, binary.LittleEndian, b)
-		if err != nil {
-			return fmt.Errorf("unable to construct binary header of new firmware volume: got %v", err)
-		}
-
-	}
-	buf := header.Bytes()
-
-	// Checksum the header
-	sum, err := uefi.Checksum16(buf[:fv.HeaderLen])
-	if err != nil {
-		return err
-	}
-	newSum := 0 - sum
-	binary.LittleEndian.PutUint16(buf[50:], newSum)
-
-	// Store the header buffer in
-	fv.SetBuf(buf)
-
-	// Build the ExtHeader
-	extHeader := new(bytes.Buffer)
-	err = binary.Write(extHeader, binary.LittleEndian, fv.FirmwareVolumeExtHeader)
-	if err != nil {
-		return fmt.Errorf("unable to construct binary extended header of new firmware volume: got %v", err)
-	}
-
-	extHeaderFile, err := uefi.CreatePadFile(uint64(uefi.FileHeaderMinLength + fv.ExtHeaderSize))
-	if err != nil {
-		return fmt.Errorf("Building ExtHeader %v", err)
-	}
-	if err := extHeaderFile.ChecksumAndAssemble(extHeader.Bytes()); err != nil {
-		return fmt.Errorf("Building ExtHeader %v", err)
-	}
-
-	// Add the extended header in a Padfile just after the header.
-	extHeaderFileBuf := extHeaderFile.Buf()
-	if err = fv.InsertFile(fv.DataOffset, extHeaderFileBuf); err != nil {
-		return fmt.Errorf("Adding ExtHeader %v", err)
-	}
-	fv.DataOffset += uint64(len(extHeaderFileBuf))
-
-	// Add empty space
-	extLen := fv.Length - fv.DataOffset
-	emptyBuf := make([]byte, extLen)
-	uefi.Erase(emptyBuf, uefi.Attributes.ErasePolarity)
-
-	// Store the buffer in
-	fv.SetBuf(append(fv.Buf(), emptyBuf...))
-
-	// Make sure DataOffset is 8 byte aligned at least.
-	fv.DataOffset = uefi.Align8(fv.DataOffset)
-
-	// Internal fields
-	fv.FVOffset = v.AbsOffset - offset
-	fv.FreeSpace = fv.Length - fv.DataOffset
-	fv.FVName = v.Name
-
 	// Add the FV
 	newElements = append(newElements, uefi.MakeTyped(fv))
 
 	// keep part of the BIOS Pad after the new fv if needed
-	if v.AbsOffset-offset-bp.Offset+v.Size < uint64(len(bpBuf)) {
-		tbp, err := uefi.NewBIOSPadding(bpBuf[v.AbsOffset-offset-bp.Offset+v.Size:], v.AbsOffset-offset+v.Size)
+	if offset-bp.Offset+fv.Length < uint64(len(bpBuf)) {
+		tbp, err := uefi.NewBIOSPadding(bpBuf[offset-bp.Offset+fv.Length:], offset+fv.Length)
 		if err != nil {
 			return err
 		}
@@ -204,6 +121,98 @@ func (v *CreateFV) createFV(br *uefi.BIOSRegion, offset uint64, bp *uefi.BIOSPad
 	}
 	br.Elements = newElements
 	return nil
+}
+
+func createEmptyFirmwareVolume(fvOffset, size uint64, name *guid.GUID) (*uefi.FirmwareVolume, error) {
+	// TODO: can this be refactored with the code in repack.go and assemble.go ?
+	fv := &uefi.FirmwareVolume{} // new Firmware Volume
+	// Set up volume header first.
+	fv.FileSystemGUID = *uefi.FFS2
+	fv.Signature = binary.LittleEndian.Uint32([]byte("_FVH"))
+	// TODO: retrieve all details from (all) other fv in BIOS Region
+	fv.Attributes = 0x0004FEFF
+	fv.Revision = 2
+	// Create Blocks
+	fv.Blocks = make([]uefi.Block, 2)
+	fv.Blocks[0] = uefi.Block{Size: 4096, Count: uint32(size / 4096)}
+	fv.Blocks[1] = uefi.Block{}
+	// Calculate the HeaderLen field
+	fv.HeaderLen = uint16(uefi.FirmwareVolumeFixedHeaderSize + int(unsafe.Sizeof(uefi.Block{}))*len(fv.Blocks))
+
+	fv.DataOffset = uint64(fv.HeaderLen) // unless we add the extended header
+	fv.Length = size
+
+	if name != nil {
+		// TODO: should the extended header offset be computed ?
+		fv.ExtHeaderOffset = 0x60
+		fv.FVName = *name
+		fv.ExtHeaderSize = uefi.FirmwareVolumeExtHeaderMinSize
+	}
+	// Generate binary header.
+	header := new(bytes.Buffer)
+	err := binary.Write(header, binary.LittleEndian, fv.FirmwareVolumeFixedHeader)
+	if err != nil {
+		return nil, fmt.Errorf("unable to construct binary header of new firmware volume: got %v", err)
+	}
+	for _, b := range fv.Blocks {
+		err = binary.Write(header, binary.LittleEndian, b)
+		if err != nil {
+			return nil, fmt.Errorf("unable to construct binary header of new firmware volume: got %v", err)
+		}
+
+	}
+	buf := header.Bytes()
+
+	// Checksum the header
+	sum, err := uefi.Checksum16(buf[:fv.HeaderLen])
+	if err != nil {
+		return nil, err
+	}
+	newSum := 0 - sum
+	binary.LittleEndian.PutUint16(buf[50:], newSum)
+
+	// Store the header buffer in
+	fv.SetBuf(buf)
+
+	if name != nil {
+		// Build the ExtHeader
+		extHeader := new(bytes.Buffer)
+		err = binary.Write(extHeader, binary.LittleEndian, fv.FirmwareVolumeExtHeader)
+		if err != nil {
+			return nil, fmt.Errorf("unable to construct binary extended header of new firmware volume: got %v", err)
+		}
+
+		extHeaderFile, err := uefi.CreatePadFile(uint64(uefi.FileHeaderMinLength + fv.ExtHeaderSize))
+		if err != nil {
+			return nil, fmt.Errorf("Building ExtHeader %v", err)
+		}
+		if err := extHeaderFile.ChecksumAndAssemble(extHeader.Bytes()); err != nil {
+			return nil, fmt.Errorf("Building ExtHeader %v", err)
+		}
+
+		// Add the extended header in a Padfile just after the header.
+		extHeaderFileBuf := extHeaderFile.Buf()
+		if err = fv.InsertFile(fv.DataOffset, extHeaderFileBuf); err != nil {
+			return nil, fmt.Errorf("Adding ExtHeader %v", err)
+		}
+		fv.DataOffset += uint64(len(extHeaderFileBuf))
+	}
+	// Add empty space
+	extLen := fv.Length - fv.DataOffset
+	emptyBuf := make([]byte, extLen)
+	uefi.Erase(emptyBuf, uefi.Attributes.ErasePolarity)
+
+	// Store the buffer in
+	fv.SetBuf(append(fv.Buf(), emptyBuf...))
+
+	// Make sure DataOffset is 8 byte aligned at least.
+	fv.DataOffset = uefi.Align8(fv.DataOffset)
+
+	// Internal fields
+	fv.FVOffset = fvOffset
+	fv.FreeSpace = fv.Length - fv.DataOffset
+
+	return fv, nil
 }
 
 func init() {
