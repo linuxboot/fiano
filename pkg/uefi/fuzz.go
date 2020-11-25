021051a9b9f861deb2f05eb8b09b21b0809ed7bb
Mark Pictor <mpictor@gmail.com>
2020-11-25 16:14:06 -0600

    fuzz
    Uses github.com/dvyukov/go-fuzz; see fuzz.go for instructions
    
    Signed-off-by: Mark Pictor <mpictor@gmail.com>

---
+++ pkg/uefi/fuzz.go
@@ -0,0 +1,41 @@
+// +build gofuzz
+
+package uefi
+
+import (
+	"log"
+)
+
+// go get github.com/dvyukov/go-fuzz/go-fuzz
+// go get github.com/dvyukov/go-fuzz/go-fuzz-build
+//
+// mkdir fuzz
+// go-fuzz-build
+// go-fuzz -bin uefi-fuzz.zip -workdir fuzz
+
+type nopWriter struct{}
+
+func (n *nopWriter) Write(_ []byte) (int, error) { return 0, nil }
+
+func init() {
+	//speed up logging
+	log.SetFlags(0)
+	log.SetOutput(&nopWriter{})
+}
+
+const (
+	ICK int = iota - 1
+	MEH
+	WOW
+)
+
+//	func Parse(buf []byte) (Firmware, error)
+func Fuzz(b []byte) int {
+	//initialize, since something could have changed the polarity
+	Attributes = ROMAttributes{ErasePolarity: poisonedPolarity}
+	_, err := Parse(b)
+	if err == nil {
+		return MEH
+	}
+	return WOW
+}
