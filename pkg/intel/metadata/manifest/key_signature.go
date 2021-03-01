5e8ddc5a1c8a6fe74d4b62da11d6d5cac5217ff6
Philipp Deppenwiese <zaolin.daisuki@gmail.com>
2021-03-01 02:45:23 +0100

    Implement km & bpm stitching of signature

--- pkg/intel/metadata/manifest/key_signature.go
+++ pkg/intel/metadata/manifest/key_signature.go
@@ -37,6 +37,7 @@ func (m *KeySignature) Verify(signedData []byte) error {
 // if signAlgo is zero then it is detected automatically, based on the type
 // of the provided private key.
 func (ks *KeySignature) SetSignature(signAlgo Algorithm, privKey crypto.Signer, signedData []byte) error {
+	ks.Version = 0x10
 	err := ks.Key.SetPubKey(privKey.Public())
 	if err != nil {
 		return fmt.Errorf("unable to set public key: %w", err)
@@ -51,10 +52,26 @@ func (ks *KeySignature) SetSignature(signAlgo Algorithm, privKey crypto.Signer, 
 // Signing algorithm will be detected automatically based on the type of the
 // provided private key.
 func (ks *KeySignature) SetSignatureAuto(privKey crypto.Signer, signedData []byte) error {
+	ks.Version = 0x10
 	err := ks.Key.SetPubKey(privKey.Public())
 	if err != nil {
 		return fmt.Errorf("unable to set public key: %w", err)
 	}
 
 	return ks.SetSignature(0, privKey, signedData)
+}
+
+// FillSignature sets a signature and all the values of KeyManifest,
+// accordingly to arguments signAlgo, pubKey and signedData.
+//
+// if signAlgo is zero then it is detected automatically, based on the type
+// of the provided private key.
+func (ks *KeySignature) FillSignature(signAlgo Algorithm, pubKey crypto.PublicKey, signedData []byte, hashAlgo Algorithm) error {
+	ks.Version = 0x10
+	err := ks.Key.SetPubKey(pubKey)
+	if err != nil {
+		return fmt.Errorf("unable to set public key: %w", err)
+	}
+
+	return ks.Signature.FillSignature(signAlgo, pubKey, signedData, hashAlgo)
 }
--- pkg/intel/metadata/manifest/signature.go
+++ pkg/intel/metadata/manifest/signature.go
@@ -66,7 +66,7 @@ func (m Signature) SignatureData() (SignatureDataInterface, error) {
 // * SignatureRSAASA
 // * SignatureECDSA
 // * SignatureSM2
-func (m *Signature) SetSignatureByData(sig SignatureDataInterface) error {
+func (m *Signature) SetSignatureByData(sig SignatureDataInterface, hashAlgo Algorithm) error {
 	err := m.SetSignatureData(sig)
 	if err != nil {
 		return err
@@ -75,19 +75,35 @@ func (m *Signature) SetSignatureByData(sig SignatureDataInterface) error {
 	switch sig := sig.(type) {
 	case SignatureRSAPSS:
 		m.SigScheme = AlgRSAPSS
-		m.HashAlg = AlgSHA256
+		if hashAlgo.IsNull() {
+			m.HashAlg = AlgSHA256
+		} else {
+			m.HashAlg = hashAlgo
+		}
 		m.KeySize.SetInBytes(uint16(len(m.Data)))
 	case SignatureRSAASA:
 		m.SigScheme = AlgRSASSA
-		m.HashAlg = AlgSHA256
+		if hashAlgo.IsNull() {
+			m.HashAlg = AlgSHA256
+		} else {
+			m.HashAlg = hashAlgo
+		}
 		m.KeySize.SetInBytes(uint16(len(m.Data)))
 	case SignatureECDSA:
 		m.SigScheme = AlgECDSA
-		m.HashAlg = AlgSHA256
+		if hashAlgo.IsNull() {
+			m.HashAlg = AlgSHA512
+		} else {
+			m.HashAlg = hashAlgo
+		}
 		m.KeySize.SetInBits(uint16(sig.R.BitLen()))
 	case SignatureSM2:
 		m.SigScheme = AlgSM2
-		m.HashAlg = AlgSM3_256
+		if hashAlgo.IsNull() {
+			m.HashAlg = AlgSM3_256
+		} else {
+			m.HashAlg = hashAlgo
+		}
 		m.KeySize.SetInBits(uint16(sig.R.BitLen()))
 	default:
 		return fmt.Errorf("unexpected signature type: %T", sig)
@@ -138,12 +154,33 @@ func (m *Signature) SetSignatureData(sig SignatureDataInterface) error {
 // if signAlgo is zero then it is detected automatically, based on the type
 // of the provided private key.
 func (m *Signature) SetSignature(signAlgo Algorithm, privKey crypto.Signer, signedData []byte) error {
+	m.Version = 0x10
 	signData, err := NewSignatureData(signAlgo, privKey, signedData)
 	if err != nil {
 		return fmt.Errorf("unable to construct the signature data: %w", err)
 	}
 
-	err = m.SetSignatureByData(signData)
+	err = m.SetSignatureByData(signData, AlgNull)
+	if err != nil {
+		return fmt.Errorf("unable to set the signature: %w", err)
+	}
+
+	return nil
+}
+
+// FillSignature sets the signature accordingly to arguments signAlgo,
+// pubKey and signedData; and sets all the fields of the structure Signature.
+//
+// if signAlgo is zero then it is detected automatically, based on the type
+// of the provided private key.
+func (m *Signature) FillSignature(signAlgo Algorithm, pubKey crypto.PublicKey, signedData []byte, hashAlgo Algorithm) error {
+	m.Version = 0x10
+	signData, err := NewSignatureByData(signAlgo, pubKey, signedData)
+	if err != nil {
+		return fmt.Errorf("unable to construct the signature data: %w", err)
+	}
+
+	err = m.SetSignatureByData(signData, hashAlgo)
 	if err != nil {
 		return fmt.Errorf("unable to set the signature: %w", err)
 	}
