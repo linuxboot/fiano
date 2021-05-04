9515f7c3ce14488fff7d351f64daa6f5928acc6c
Dmitrii Okunev <xaionaro@fb.com>
2021-05-04 21:06:13 +0100

    feat(fit): Add methids to extract KM and BPM directly

--- pkg/intel/metadata/fit/entry_key_manifest_record.go
+++ pkg/intel/metadata/fit/entry_key_manifest_record.go
@@ -7,7 +7,7 @@ import (
 	"github.com/9elements/converged-security-suite/v2/pkg/intel/metadata/manifest/key"
 )
 
-// Parse creates EntryKeyManifestRecord from EntryKeyManifest
+// ParseData creates EntryKeyManifestRecord from EntryKeyManifest
 func (entry *EntryKeyManifestRecord) ParseData() (*key.Manifest, error) {
 	var km key.Manifest
 	_, err := km.ReadFrom(bytes.NewReader(entry.GetDataBytes()))
@@ -15,4 +15,15 @@ func (entry *EntryKeyManifestRecord) ParseData() (*key.Manifest, error) {
 		return nil, fmt.Errorf("failed to parse KeyManifest, err: %v", err)
 	}
 	return &km, nil
+}
+
+// ParseKeyManifest returns a boot policy manifest if it was able to
+// parse one.
+func (table Table) ParseKeyManifest(firmware []byte) (*key.Manifest, error) {
+	hdr := table.First(EntryTypeKeyManifestRecord)
+	if hdr == nil {
+		return nil, ErrNotFound{}
+	}
+
+	return hdr.GetEntry(firmware).(*EntryKeyManifestRecord).ParseData()
 }
