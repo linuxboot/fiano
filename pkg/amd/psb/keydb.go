887d9ac5f2fb73061eb76f0ad5087b7a0927a76d
Marco Guerri <marcoguerri@fb.com>
2021-09-02 09:21:24 -0700

    [attestation][amd] Add support for extracting AMD Public Key from firmware
    AMD key is the root that gives trust to several other keys on the
    system. Before being able to validate any signature on any blob, we need to
    be able to parse AMD key.
    
    Signed-off-by: Dmitrii Okunev <xaionaro@meta.com>

--- pkg/amd/psb/keydb.go
+++ pkg/amd/psb/keydb.go
@@ -1,8 +1,27 @@
 package psb
 
-import "fmt"
+import (
+	"fmt"
 
-// ShowKeyDB extracts key database from firmware images and shows all keys contained into it
-func ShowKeyDB() {
-	fmt.Println("KeyDB: <...>")
+	amd_manifest "github.com/9elements/converged-security-suite/pkg/amd/manifest"
+)
+
+const (
+	// AMDPublicKeyEntry denotes AMD public key entry in PSP Directory table
+	AMDPublicKeyEntry amd_manifest.PSPDirectoryTableEntryType = 0x00
+)
+
+// GetKeyDB extracts the key database from firmware image
+func GetKeyDB(firmware amd_manifest.Firmware) error {
+	pspFw, err := amd_manifest.ParsePSPFirmware(firmware)
+	if err != nil {
+		return fmt.Errorf("could not get key database: %w", err)
+	}
+
+	key, err := extractAMDPublicKey(pspFw, firmware)
+	if err != nil {
+		return fmt.Errorf("could no extract AMD public key from firmware: %w", err)
+	}
+	fmt.Println(key.String())
+	return nil
 }
