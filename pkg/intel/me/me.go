5972e1645519526fd674ba17c9eb00f81cf02a74
Christopher Meis <christopher.meis@9elements.com>
2021-09-08 08:09:38 +0200

    Refactor new me parsing Add tests for me parsing
    Signed-off-by: Christopher Meis <christopher.meis@9elements.com>

--- pkg/intel/me/me.go
+++ pkg/intel/me/me.go
@@ -7,263 +7,147 @@ package me
 import (
 	"bytes"
 	"encoding/binary"
-	"fmt"
-	"strings"
-
-	"github.com/linuxboot/fiano/pkg/uefi"
+	"io"
 )
 
-// LegacyFlashPartitionTableHeader describes the old flash partition table header
-// in Intel ME binaries.
-type LegacyFlashPartitionTableHeader struct {
-	Padding        [16]uint8 // 16 zeros
-	Marker         uint32    // Always $FPT
-	NumFptEntries  uint32
-	HeaderVersion  uint8
-	EntryVersion   uint8
-	HeaderLength   uint8 // Usually 0x30
-	HeaderChecksum uint8
-	TicksToAdd     uint16
-	TokensToAdd    uint16
-	UMASize        uint32
-	Flags          uint32
-}
+var (
+	Signature = [4]byte{0x24, 0x46, 0x50, 0x54}
+)
 
-func (h LegacyFlashPartitionTableHeader) String() string {
-	var b strings.Builder
-	b.WriteString("Flash partition table:\n")
-	fmt.Fprintf(&b, " Entries       : %d\n", h.NumFptEntries)
-	fmt.Fprintf(&b, " HeaderVersion : 0x%x\n", h.HeaderVersion)
-	fmt.Fprintf(&b, " EntryVersion  : 0x%x\n", h.EntryVersion)
-	fmt.Fprintf(&b, " HeaderLength  : 0x%x\n", h.HeaderLength)
-	fmt.Fprintf(&b, " HeaderChecksum: 0x%x\n", h.HeaderChecksum)
-	fmt.Fprintf(&b, " TicksToAdd    : 0x%x\n", h.TicksToAdd)
-	fmt.Fprintf(&b, " TokensToAdd   : 0x%x\n", h.TokensToAdd)
-	fmt.Fprintf(&b, " UMASize       : 0x%x\n", h.UMASize)
-	fmt.Fprintf(&b, " Flags         : 0x%x\n", h.Flags)
-
-	return b.String()
-}
-
-// FlashPartitionTableHeader describes the new flash partition table header
-// in Intel ME binaries.
-type FlashPartitionTableHeader struct {
-	Marker             uint32 // Always $FPT
-	NumFptEntries      uint32
-	HeaderVersion      uint8 // Only support 2.0
-	EntryVersion       uint8
-	HeaderLength       uint8 // Usually 0x20
-	HeaderChecksum     uint8
-	TicksToAdd         uint16
-	TokensToAdd        uint16
-	UMASizeOrReserved  uint32
-	FlashLayoutOrFlags uint32
-	// Not Present in ME version 7
-	FitcMajor  uint16
-	FitcMinor  uint16
-	FitcHotfix uint16
-	FitcBuild  uint16
-}
-
-func (h FlashPartitionTableHeader) String() string {
-	var b strings.Builder
-
-	b.WriteString("Flash partition table:\n")
-	fmt.Fprintf(&b, " Entries            : %d\n", h.NumFptEntries)
-	fmt.Fprintf(&b, " HeaderVersion      : 0x%x\n", h.HeaderVersion)
-	fmt.Fprintf(&b, " EntryVersion       : 0x%x\n", h.EntryVersion)
-	fmt.Fprintf(&b, " HeaderLength       : 0x%x\n", h.HeaderLength)
-	fmt.Fprintf(&b, " HeaderChecksum     : 0x%x\n", h.HeaderChecksum)
-	fmt.Fprintf(&b, " TicksToAdd         : 0x%x\n", h.TicksToAdd)
-	fmt.Fprintf(&b, " TokensToAdd        : 0x%x\n", h.TokensToAdd)
-	fmt.Fprintf(&b, " UMASizeOrReserved  : 0x%x\n", h.UMASizeOrReserved)
-	fmt.Fprintf(&b, " FlashLayoutOrFlags : 0x%x\n", h.FlashLayoutOrFlags)
-	fmt.Fprintf(&b, " Fitc Version       : %d.%d.%d.%d\n", h.FitcMajor, h.FitcMinor, h.FitcHotfix, h.FitcBuild)
-
-	return b.String()
-}
-
-// FlashPartitionTableEntry describes information of a flash partition table entry.
-type FlashPartitionTableEntry struct {
-	Name           [4]uint8
-	Owner          [4]uint8
-	Offset         uint32
-	Length         uint32
-	StartTokens    uint32
-	MaxTokens      uint32
-	ScratchSectors uint32
-	Flags          uint32
-}
-
-func (e FlashPartitionTableEntry) String() string {
-	var b strings.Builder
-	b.WriteString("Flash partition entry:\n")
-	fmt.Fprintf(&b, " Name          : %s\n", []byte{e.Name[0], e.Name[1], e.Name[2], e.Name[3]})
-	fmt.Fprintf(&b, " Owner         : %s\n", []byte{e.Owner[0], e.Owner[1], e.Owner[2], e.Owner[3]})
-	fmt.Fprintf(&b, " Offset        : 0x%x\n", e.Offset)
-	fmt.Fprintf(&b, " Length        : 0x%x\n", e.Length)
-	fmt.Fprintf(&b, " StartTokens   : 0x%x\n", e.StartTokens)
-	fmt.Fprintf(&b, " MaxTokens     : 0x%x\n", e.MaxTokens)
-	fmt.Fprintf(&b, " ScratchSectors: 0x%x\n", e.ScratchSectors)
-	fmt.Fprintf(&b, " Flags         : 0x%x\n", e.Flags)
-
-	if e.Flags>>24 == 0xff {
-		b.WriteString(" Valid         : No\n")
-	} else {
-		b.WriteString(" Valid         : yes\n")
+func parseLegacyFlashPartitionTableHeader(r io.Reader) (*LegacyFlashPartitionTableHeader, error) {
+	var header LegacyFlashPartitionTableHeader
+	var scrap [12]byte
+	if err := binary.Read(r, binary.LittleEndian, &scrap); err != nil {
+		return nil, err
 	}
-	if e.Flags&1 > 0 {
-		b.WriteString(" Partition     : Data\n")
-	} else {
-		b.WriteString(" Partition     : Code\n")
+	if err := binary.Read(r, binary.LittleEndian, &header.Marker); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.NumFptEntries); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.HeaderVersion); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.EntryVersion); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.HeaderLength); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.HeaderChecksum); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.TicksToAdd); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.TokensToAdd); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.UMASize); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.Flags); err != nil {
+		return nil, err
+	}
+	return &header, nil
+
+}
+
+func parseFlashPartitionTableHeader(r io.Reader) (*FlashPartitionTableHeader, error) {
+	var header FlashPartitionTableHeader
+	// Set Signature
+	header.Marker = Signature
+
+	if err := binary.Read(r, binary.LittleEndian, &header.NumFptEntries); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.HeaderVersion); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.EntryVersion); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.HeaderLength); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.HeaderChecksum); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.TicksToAdd); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.TokensToAdd); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.UMASizeOrReserved); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.FlashLayoutOrFlags); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.FitcMajor); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.FitcMinor); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.FitcHotfix); err != nil {
+		return nil, err
+	}
+	if err := binary.Read(r, binary.LittleEndian, &header.FitcBuild); err != nil {
+		return nil, err
 	}
 
-	return b.String()
+	return &header, nil
 }
 
-// IntelME abstracts the ME/CSME/SPS firmware found on intel platforms
-type IntelME struct {
-	hdr        *FlashPartitionTableHeader
-	legacyhdr  *LegacyFlashPartitionTableHeader
-	legacy     bool
-	partitions []FlashPartitionTableEntry
-	image      []byte
-	// Offset in image to $FPT
-	fptoffset uint32
+func parseEntry(r io.Reader) (*FlashPartitionTableEntry, error) {
+	var entry FlashPartitionTableEntry
+	if err := binary.Read(r, binary.LittleEndian, &entry); err != nil {
+		return nil, err
+	}
+	return &entry, nil
 }
 
 // ParseIntelFirmware parses the Intel firmware image by uefi.Firmware interface`
-func ParseIntelFirmware(firmware uefi.Firmware) (*IntelME, error) {
-	uefi, err := ParseIntelFirmwareBytes(firmware.Buf())
-	if err != nil {
-		return nil, fmt.Errorf("unable to get the content of file: %v", err)
+func ParseIntelME(r io.Reader) (*IntelME, error) {
+	var me IntelME
+	me.legacy = false
+	var numEntries uint32
+
+	// Read first 4 byte, we catch the marker as prefix or suffix
+	var markerarea [4]byte
+	if err := binary.Read(r, binary.LittleEndian, &markerarea); err != nil {
+		return nil, err
 	}
 
-	return uefi, nil
-}
-
-// ParseIntelFirmwareBytes parses the Intel firmware image from bytes
-func ParseIntelFirmwareBytes(imageBytes []byte) (*IntelME, error) {
-	legacy := false
-	fptoffset := -1
-	// Search for the Flash partition table
-	for i := 0; i < len(imageBytes); i += 0x1000 {
-
-		// New Header
-		if bytes.HasPrefix(imageBytes[i:], []byte(`$FPT`)) {
-			fptoffset = i
-			break
-		}
-		// Legacy Header
-		if bytes.HasPrefix(imageBytes[i:], append(make([]byte, 16), []byte(`$FPT`)...)) {
-			legacy = true
-			fptoffset = i
-			break
-		}
-	}
-	if fptoffset == -1 {
-		return nil, fmt.Errorf("no FlashPartitionTable found")
-	}
-
-	me := &IntelME{image: imageBytes, legacy: legacy, fptoffset: uint32(fptoffset)}
-	reader := bytes.NewReader(imageBytes[fptoffset:])
-	offset := 0
-	entries := 0
-
-	if legacy {
-		if err := binary.Read(reader, binary.LittleEndian, &me.legacyhdr); err != nil {
+	// Check on new header
+	if bytes.HasPrefix(markerarea[:], Signature[:]) {
+		hdr, err := parseFlashPartitionTableHeader(r)
+		if err != nil {
 			return nil, err
 		}
-		if me.legacyhdr.HeaderVersion != 0x20 {
-			return nil, fmt.Errorf("unsupported header version. Got 0x%x", me.legacyhdr.HeaderVersion)
-		}
-		if int(me.legacyhdr.HeaderLength) > len(imageBytes)-fptoffset {
-			return nil, fmt.Errorf("invalid header length. Got 0x%x", me.legacyhdr.HeaderLength)
-		}
-		offset = int(me.legacyhdr.HeaderLength)
-		entries = int(me.legacyhdr.NumFptEntries)
+		me.hdr = hdr
+		numEntries = hdr.NumFptEntries
 	} else {
-		if err := binary.Read(reader, binary.LittleEndian, &me.hdr); err != nil {
+		me.legacy = true
+		hdr, err := parseLegacyFlashPartitionTableHeader(r)
+		if err != nil {
 			return nil, err
 		}
-		if me.hdr.HeaderVersion != 0x20 {
-			return nil, fmt.Errorf("unsupported header version. Got 0x%x", me.hdr.HeaderVersion)
-		}
-		if int(me.hdr.HeaderLength) > len(imageBytes)-fptoffset {
-			return nil, fmt.Errorf("invalid header length. Got 0x%x", me.hdr.HeaderLength)
-		}
-		offset = int(me.hdr.HeaderLength)
-		entries = int(me.hdr.NumFptEntries)
+		me.legacyhdr = hdr
+		numEntries = hdr.NumFptEntries
 	}
 
-	reader = bytes.NewReader(imageBytes[fptoffset+offset:])
-
-	for i := 0; i < entries; i++ {
-		var e FlashPartitionTableEntry
-		if err := binary.Read(reader, binary.LittleEndian, &e); err != nil {
+	partitions := make([]FlashPartitionTableEntry, 0)
+	for i := uint32(0); i < numEntries; i++ {
+		entry, err := parseEntry(r)
+		if err != nil {
 			return nil, err
 		}
-		me.partitions = append(me.partitions, e)
-
+		partitions = append(partitions, *entry)
 	}
-
-	return me, nil
-}
-
-// ImageBytes just returns the image as `[]byte`.
-func (m *IntelME) ImageBytes() []byte {
-	return m.image
-}
-
-// PrintInfo prints the ME partitions in human readable format
-func (m *IntelME) PrintInfo() string {
-	ret := ""
-	if m.legacy {
-		ret += m.legacyhdr.String()
-	} else {
-		ret += m.hdr.String()
-	}
-	for i := range m.partitions {
-		ret += m.partitions[i].String()
-	}
-	return ret
-}
-
-// WritePartition writes new data into specified partition.
-// Must be equal in size to current parition
-func (m *IntelME) WritePartition(id string, data []byte) (err error) {
-	for i := range m.partitions {
-		name := m.partitions[i].Name
-		if string(bytes.Trim([]byte{name[0], name[1], name[2], name[3]}, "\x00")) == id {
-			if uint32(len(data)) != m.partitions[i].Length {
-				return fmt.Errorf("invalid length")
-			}
-			m.image = append(append(m.image[:m.partitions[i].Offset+m.fptoffset], data...),
-				m.image[m.partitions[i].Offset+m.partitions[i].Length+m.fptoffset:]...)
-			return nil
-		}
-	}
-	return fmt.Errorf("not found")
-}
-
-// ReadPartition reads data from specified partition.
-func (m *IntelME) ReadPartition(id string) (data []byte, err error) {
-	for i := range m.partitions {
-		name := m.partitions[i].Name
-		if string(bytes.Trim([]byte{name[0], name[1], name[2], name[3]}, "\x00")) == id {
-			data = m.image[m.partitions[i].Offset+m.fptoffset : m.partitions[i].Offset+m.partitions[i].Length+m.fptoffset]
-			return data, nil
-		}
-	}
-	return nil, fmt.Errorf("not found")
-}
-
-// LsPartitions list all partition found in image
-func (m *IntelME) LsPartitions() []string {
-	var part []string
-	for i := range m.partitions {
-		name := m.partitions[i].Name
-		part = append(part, string(bytes.Trim([]byte{name[0], name[1], name[2], name[3]}, "\x00")))
-	}
-	return part
+	me.partitions = partitions
+	return &me, nil
 }
