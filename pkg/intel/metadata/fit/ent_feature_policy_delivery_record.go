da981c586c2fb6a2be18acf0acf4fb78f614c598
Dmitrii Okunev <xaionaro@gmail.com>
2022-01-10 15:02:43 +0000

    Revert "Add Intel FIT parser (#349)"
    This reverts commit c902f7e687c3fd0b1774e816f3f18cdd7258c936.
    
    Signed-off-by: Dmitrii Okunev <xaionaro@fb.com>

--- pkg/intel/metadata/fit/ent_feature_policy_delivery_record.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2017-2021 the LinuxBoot Authors. All rights reserved
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-package fit
-
-// EntryFeaturePolicyDeliveryRecord represents a FIT entry of type "Feature Policy Delivery Record" (0x2D)
-type EntryFeaturePolicyDeliveryRecord struct{ EntryBase }
