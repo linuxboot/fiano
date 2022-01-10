ccd71a7796feb8202373bf50561ca0a4558c7f26
Dmitrii Okunev <xaionaro@gmail.com>
2022-01-10 15:02:43 +0000

    Revert "Add Intel FIT parser (#349)"
    This reverts commit c902f7e687c3fd0b1774e816f3f18cdd7258c936.
    
    Signed-off-by: Dmitrii Okunev <xaionaro@fb.com>

--- pkg/intel/metadata/fit/ent_bios_startup_module_entry.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2017-2021 the LinuxBoot Authors. All rights reserved
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-package fit
-
-// EntryBIOSStartupModuleEntry represents a FIT entry of type "BIOS Startup Module Entry" (0x07)
-type EntryBIOSStartupModuleEntry struct{ EntryBase }
--- pkg/intel/metadata/fit/ent_unknown.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2017-2021 the LinuxBoot Authors. All rights reserved
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-package fit
-
-// EntryUnknown represents an unknown FIT entry type.
-type EntryUnknown struct{ EntryBase }
