7202faefe9272f2ec5d4ccd58d10ebe420402dff
Philipp Deppenwiese <zaolin.daisuki@gmail.com>
2022-12-30 20:49:24 +0100

    code generation, move readme, adapt template
    Signed-off-by: Philipp Deppenwiese <zaolin.daisuki@gmail.com>

---
+++ pkg/intel/metadata/bg/key/manifest_test.go
@@ -0,0 +1,15 @@
+// Copyright 2017-2023 the LinuxBoot Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package key
+
+import (
+	"testing"
+
+	"github.com/linuxboot/fiano/pkg/intel/metadata/common/unittest"
+)
+
+func TestReadWrite(t *testing.T) {
+	unittest.ManifestReadWrite(t, &Manifest{}, "testdata/km.bin")
+}
