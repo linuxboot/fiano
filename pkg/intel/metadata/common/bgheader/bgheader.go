a693ce1f3dedc0b57c1a31e64f56865b92bf4407
Philipp Deppenwiese <zaolin.daisuki@gmail.com>
2023-01-04 19:24:42 +0100

    Add bgheader pkg for header detection
    Signed-off-by: Philipp Deppenwiese <zaolin.daisuki@gmail.com>

---
+++ pkg/intel/metadata/common/bgheader/bgheader.go
@@ -0,0 +1,51 @@
+package bgheader
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+)
+
+var (
+	binaryOrder = binary.LittleEndian
+)
+
+type structInfo struct {
+	ID      structureID `json:"StructInfoID"`
+	Version uint8       `json:"StructInfoVersion"`
+}
+
+type structureID [8]byte
+
+type BootGuardVersion uint8
+
+const (
+	Version10 BootGuardVersion = 1
+	Version20 BootGuardVersion = 2
+)
+
+func (bgv BootGuardVersion) String() string {
+	switch bgv {
+	case Version10:
+		return "1.0"
+	case Version20:
+		return "2.0"
+	}
+	return "unknown"
+}
+
+func DetectBGV(r io.Reader) (BootGuardVersion, error) {
+	var s structInfo
+	var bgv BootGuardVersion
+
+	err := binary.Read(r, binaryOrder, s.ID[:])
+	if err != nil {
+		return 0, fmt.Errorf("unable to read field 'ID': %w", err)
+	}
+	err = binary.Read(r, binaryOrder, bgv)
+	if err != nil {
+		return 0, fmt.Errorf("unable to read field 'Version': %w", err)
+	}
+
+	return bgv, nil
+}
