5e3247077f9de14f4bed85eacf7522a3f739c06f
Philipp Deppenwiese <zaolin.daisuki@gmail.com>
2023-01-31 12:51:37 +0100

    Add defer seek to bgheader pkg and add previous parse key and bootpolicy manifest ParseData() functions
    Signed-off-by: Philipp Deppenwiese <zaolin.daisuki@gmail.com>

--- pkg/intel/metadata/fit/ent_boot_policy_manifest.go
+++ pkg/intel/metadata/fit/ent_boot_policy_manifest.go
@@ -6,7 +6,13 @@ package fit
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
 	"io"
+
+	"github.com/linuxboot/fiano/pkg/intel/metadata/bg/bgbootpolicy"
+	"github.com/linuxboot/fiano/pkg/intel/metadata/cbnt/cbntbootpolicy"
+	"github.com/linuxboot/fiano/pkg/intel/metadata/common/bgheader"
 )
 
 // EntryBootPolicyManifestRecord represents a FIT entry of type "Boot Policy Manifest" (0x0C)
@@ -32,4 +38,42 @@ func (entry *EntryBootPolicyManifestRecord) CustomRecalculateHeaders() error {
 // Reader creates io.ReadSeeker from EntryBootPolicyManifestRecord
 func (entry *EntryBootPolicyManifestRecord) Reader() *bytes.Reader {
 	return bytes.NewReader(entry.DataSegmentBytes)
+}
+
+// ParseData creates EntryBootPolicyManifestRecord from EntryBootPolicyManifest
+func (entry *EntryBootPolicyManifestRecord) ParseData() (*bgbootpolicy.Manifest, *cbntbootpolicy.Manifest, error) {
+	r := bytes.NewReader(entry.DataSegmentBytes)
+	version, err := bgheader.DetectBGV(r)
+	if err != nil {
+		return nil, nil, err
+	}
+	switch version {
+	case bgheader.Version10:
+		manifest := bgbootpolicy.NewManifest()
+		_, err = manifest.ReadFrom(r)
+		if err != nil && !errors.Is(err, io.EOF) {
+			return nil, nil, err
+		}
+		return manifest, nil, nil
+	case bgheader.Version20:
+		manifest := cbntbootpolicy.NewManifest()
+		_, err = manifest.ReadFrom(r)
+		if err != nil && !errors.Is(err, io.EOF) {
+			return nil, nil, err
+		}
+		return nil, manifest, nil
+	default:
+		return nil, nil, fmt.Errorf("failed to parse BootPolicyManifest, err: %v", err)
+	}
+}
+
+// ParseBootPolicyManifest returns a boot policy manifest if it was able to
+// parse one.
+func (table Table) ParseBootPolicyManifest(firmware []byte) (*bgbootpolicy.Manifest, *cbntbootpolicy.Manifest, error) {
+	hdr := table.First(EntryTypeBootPolicyManifest)
+	if hdr == nil {
+		return nil, nil, ErrNotFound{}
+	}
+
+	return hdr.GetEntry(firmware).(*EntryBootPolicyManifestRecord).ParseData()
 }
