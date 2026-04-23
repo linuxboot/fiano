f6c4197c4ada13bcb1e03e252f956a14bf3a702a
Michal Gorlas <michal.gorlas@9elements.com>
2026-04-23 15:38:19 +0200

    chore(intel/metadata): add BPM structure's ids
    Moved these out to the separate file for readabilty.
    
    Signed-off-by: Michal Gorlas <michal.gorlas@9elements.com>

---
+++ pkg/intel/metadata/cbnt/bootpolicy/consts.go
@@ -0,0 +1,39 @@
+// Copyright 2017-2026 the LinuxBoot Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package cbntbootpolicy
+
+const (
+	// StructureIDBPMH is the StructureID (in terms of
+	// the document #575623) of element 'BPMH'.
+	StructureIDBPMH = "__ACBP__"
+
+	// StructureIDPCD is the StructureID (in terms of
+	// the document #575623) of element 'PCD'.
+	StructureIDPCD = "__PCDS__"
+
+	// StructureIDPDRS is the StructureID of the PDRS
+	// record embedded in PCD data.
+	StructureIDPDRS = "__PDRS__"
+
+	// StructureIDCNBS is the StructureID of the CNBS
+	// record embedded in PCD data.
+	StructureIDCNBS = "__CNBS__"
+
+	// StructureIDPM is the StructureID (in terms of
+	// the document #575623) of element 'PM'.
+	StructureIDPM = "__PMDA__"
+
+	// StructureIDSE is the StructureID (in terms of
+	// the document #575623) of element 'SE'.
+	StructureIDSE = "__IBBS__"
+
+	// StructureIDSignature is the StructureID (in terms of
+	// the document #575623) of element 'Signature'.
+	StructureIDSignature = "__PMSG__"
+
+	// StructureIDTXT is the StructureID (in terms of
+	// the document #575623) of element 'TXT'.
+	StructureIDTXT = "__TXTS__"
+)
